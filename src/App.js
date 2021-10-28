2063ec4ee246c3cbc9c496eb4c85c2be5389775e
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2021-10-28 22:07:08 +0800

    UsetheReact
    使用react编写了此网站

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+              <h1>Welcome to the EADJ</h1>
+              <p>It's a Entryption and Detryption Program</p>
+              <p>And I use the react write it</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
